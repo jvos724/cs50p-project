e8a03f0a98bda75e38aa306aef04b97f89d20f6d
Jeff Vos <bugs_slights0t@icloud.com>
2024-07-15 03:26:49 -0400

    NotesDB Class - Updated docstrings - Refactored some unnecessarily complicated code

--- notesdb.py
+++ notesdb.py
@@ -5,8 +5,21 @@ from note import Note
 
 
 class NotesDB:
-    """Class representing a SQLite DB for a collection of Notes"""
+    """Class representing a SQLite DB for a collection of Notes
+
+    Attributes:
+        db_file (str): The path to the SQLite file
+        conn (sqlite3.Connection): The connection to the SQLite DB
+        cursor (sqlite3.Cursor): The cursor for the SQLite DB
+    """
+
     def __init__(self, db_file: str = ":memory:") -> None:
+        """Initialize the NoteDB instance with a SQLite file
+
+        Args:
+            db_file (str): The path to the SQLite file. Defaults to in-memory database
+        """
+
         self.db_file = db_file
         self.conn = sqlite3.connect(self.db_file)
         self.cursor = self.conn.cursor()
@@ -29,7 +42,12 @@ class NotesDB:
         self.conn.commit()
 
     def add(self, notes: list[Note]) -> None:
-        """Save a list of one or more notes to the DB"""
+        """Save a list of one or more notes to the DB
+
+        Args:
+            notes (List[Note]): the list of notes to be added
+        """
+
         query = """
         INSERT INTO notes (name, tags, content) VALUES (?, ?, ?)
         """
@@ -42,7 +60,15 @@ class NotesDB:
             self.conn.commit()
 
     def get(self, n: int = 0) -> list[Note]:
-        """Get all or n=# most recent Notes from DB"""
+        """Get all or the n most recent Notes from the DB
+
+        Args:
+            n (int): The number of most recent notes to retrieve. Defaults to 0.
+
+        Returns:
+            List[Note]: A list of retrieved Notes
+        """
+
         query = """
         SELECT * FROM notes ORDER BY id DESC
         """
@@ -55,11 +81,7 @@ class NotesDB:
 
         rows = self.cursor.fetchall()
 
-        notes = []
-        for row in rows:
-            note = Note.from_sql(row)
-            notes.append(note)
-
+        notes = [Note.from_sql(row) for row in rows]
         notes.reverse()
 
         return notes
@@ -70,14 +92,16 @@ class NotesDB:
 
     @db_file.setter
     def db_file(self, f: str) -> None:
-        """db_file setter that handles `:memory:` and creates paths"""
+        """Setter for the db_file attribute that handles `:memory:` and creates paths
+
+        Args:
+            f (str): The path to the SQLite file
+        """
         if f == ":memory:":
             self._db_file = f
         else:
             path = os.path.expanduser(f)
             dir = os.path.dirname(path)
-            if os.path.exists(dir):
-                self._db_file = path
-            else:
-                os.mkdir(dir)
-                self._db_file = path
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            self._db_file = path
